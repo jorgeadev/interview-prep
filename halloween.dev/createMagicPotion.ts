682842d24478edc9723d0111b709e15c464d07df
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2024-11-12 01:44:32 -0500

    Add solution at first challenge

--- halloween.dev/createMagicPotion.ts
+++ halloween.dev/createMagicPotion.ts
@@ -1,12 +1,60 @@
-const potions = [4, 5, 6, 2];
-const goal = 8;
+const potions = [1, 4, 3, 6]
+const goal = 7
+
+/*function createMagicPotion(potions: number[], target: number) {
+	let solutions: [number, number][] = [];
+	for (let i = 0; i < potions.length; i++) {
+		const comp = target - potions[i];
+		const index = potions.indexOf(comp);
+		if (potions.includes(comp) && index > i) {
+			solutions.push([ i, index ]);
+		}
+	}
+	if (solutions.length === 1) {
+		return solutions[0];
+	} else if (solutions.length > 1) {
+		let minIndex = potions.length;
+		solutions.forEach((solution) => {
+			if (solution[1] < minIndex) {
+				minIndex = solution[1];
+			}
+		});
+		return solutions.find((solution: number[]) => solution[1] === minIndex);
+	}
+}*/
+
+/*function createMagicPotion(potions: number[], target: number) {
+	const solutions: [number, number][] = [];
+	const potionMap = new Map<number, number>();
+	
+	for (let i = 0; i < potions.length; i++) {
+		const comp = target - potions[i];
+		if (potionMap.has(comp)) {
+			solutions.push([potionMap.get(comp)!, i]);
+		}
+		potionMap.set(potions[i], i);
+	}
+	
+	if (solutions.length === 1) {
+		return solutions[0];
+	} else if (solutions.length > 1) {
+		return solutions.reduce((minSolution, solution) =>
+			solution[1] < minSolution[1] ? solution : minSolution
+		);
+	}
+}*/
 
 function createMagicPotion(potions: number[], target: number) {
-	// Code here
+	const potionMap = new Map<number, number>();
 	
-	console.log("Potions:", potions);
-	console.log("Goal:", target);
-	return undefined;
+	for (let i = 0; i < potions.length; i++) {
+		const comp = target - potions[i];
+		if (potionMap.has(comp)) {
+			return [potionMap.get(comp)!, i];
+		}
+		potionMap.set(potions[i], i);
+		console.log(potionMap);
+	}
 }
 
-createMagicPotion(potions, goal);
+console.log(createMagicPotion(potions, goal));
