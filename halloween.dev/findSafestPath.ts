6d6bf2270d3b0800eaf4c9f624b695244d053526
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2024-11-12 12:39:16 -0500

    Add third challenge solution

---
+++ halloween.dev/findSafestPath.ts
@@ -0,0 +1,24 @@
+const dream = [
+	[1, 3, 1],
+	[1, 5, 1],
+	[4, 2, 1],
+]
+
+function findSafestPath(dream: number[][]) {
+	const rowsNumber = dream.length
+	const colsNumber = dream[0].length
+	const dangersLevels = [];
+	dangersLevels[0] = dream[0][0];
+	for (let i = 1; i < colsNumber; i++) {
+		dangersLevels[i] = dangersLevels[i - 1] + dream[0][i];
+	}
+	for (let i = 1; i < rowsNumber; i++) {
+		dangersLevels[0] += dream[i][0];
+		for (let j = 1; j < colsNumber; j++) {
+			dangersLevels[j] = Math.min(dangersLevels[j - 1], dangersLevels[j]) + dream[i][j];
+		}
+	}
+	return dangersLevels[colsNumber - 1];
+}
+
+console.log(findSafestPath(dream));
