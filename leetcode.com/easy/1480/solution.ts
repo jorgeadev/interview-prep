d0b4e7ca4e4dfadb11e28005e3769a44ea336b0a
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2024-11-13 01:06:10 -0500

    Update code

--- leetcode.com/easy/1480/solution.ts
+++ leetcode.com/easy/1480/solution.ts
@@ -2,20 +2,16 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-/*
-const runningSum = function (nums: number[]): number[] {
+/*const runningSum = function (nums: number[]): number[] {
 	let sum = [nums[0]];
 	for (let i = 1; i < nums.length; i++) {
-		sum.push(sum[i -1] + nums[i]);
+		sum[i] = sum[i -1] + nums[i];
 	}
 	return sum;
-};
-*/
+};*/
 
 const runningSum = (nums: number[]): number[] => {
 	return nums.reduce((acc, curr, index) => acc.concat(acc[index] + curr), [0]).slice(1);
 };
-
-
 
 console.log(runningSum([1, 2, 3, 4])); // [1, 3, 6, 10]
