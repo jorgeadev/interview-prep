98787ddad77ee5a4bb23a59341e5ebbb7403cb96
Jorge Gomez <jorgealbertogomezgomez77@gmail.com>
2024-11-22 18:47:51 -0500

    Add challenge solution to battleHorde.ts

--- halloween.dev/typescript/02-battleHorde.ts
+++ halloween.dev/typescript/02-battleHorde.ts
@@ -1,7 +1,7 @@
 const zombies = "242";
 const humans = "334";
 
-function battleHorde(zombies: string, humans: string) {
+/*function battleHorde(zombies: string, humans: string) {
 	let z = 0, h = 0;
 	for (let i = 0; i < zombies.length; i++) {
 		h += Number(humans[i]);
@@ -9,7 +9,7 @@ function battleHorde(zombies: string, humans: string) {
 	}
 	if (z === h) return "x";
 	return `${ Math.abs(z - h) }${ z > h ? "z" : "h" }`;
-}
+}*/
 
 /*function battleHorde(zombies: string, humans: string) {
 	const z = zombies.split('').reduce((acc, val) => acc + parseInt(val), 0);
@@ -19,5 +19,17 @@ function battleHorde(zombies: string, humans: string) {
 	if (z === h) return "x"
 	return `${ Math.abs(z - h) }${ letter }`;
 }*/
+
+const battleHorde = (zombies: string, humans: string) => {
+	let powerDifference: number = 0;
+	for (let i = 0; i < zombies.length; i++) {
+		powerDifference += Number(zombies[i]) - Number(humans[i]);
+	}
+	if (powerDifference === 0) {
+		return "x";
+	} else {
+		return `${ Math.abs(powerDifference) }${ powerDifference > 0 ? "z" : "h" }`;
+		}
+};
 
 console.log(battleHorde(zombies, humans));
